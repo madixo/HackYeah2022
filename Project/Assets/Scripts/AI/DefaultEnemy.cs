13c335e0e042bb4ab1e5a08f852e25f7b5cf2ec5
Szymon Kurowski <szymon10001000k@gmail.com>
2022-11-20 09:53:31 +0100

    Fix slash animation, update scenes, add videos folder

---
+++ Project/Assets/Scripts/AI/DefaultEnemy.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultEnemy : MonoBehaviour
+{
+    [SerializeField]
+    private Transform probe;
+    [SerializeField]
+    private float radius;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    private bool lastState;
+    [SerializeField]
+    private bool movingRight = false;
+    [SerializeField]
+    private Rigidbody2D rigidBody;
+    [SerializeField]
+    private float speed;
+    // private bool canFlip = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void Flip() {
+
+        movingRight = !movingRight;
+        Vector3 scale = rigidBody.transform.localScale;
+        scale.x *= -1;
+        rigidBody.transform.localScale = scale;
+
+    }
+
+    void FixedUpdate() {
+
+        Collider2D leftCollider = Physics2D.OverlapCircle(probe.position, radius, whatIsGround);
+
+        if(leftCollider == null) //{
+            // if(canFlip) {
+                Flip();
+                // canFlip = false;
+            // }
+        // }else {
+            // if(!canFlip)
+                // canFlip = true;
+        // }
+
+        rigidBody.velocity = new Vector2(movingRight ? speed : -speed, rigidBody.velocity.y);
+
+    }
+}
--- Project/Assets/Scripts/Player/CameraManager.cs
+++ Project/Assets/Scripts/Player/CameraManager.cs
@@ -7,11 +7,6 @@ public class CameraManager : MonoBehaviour
     public Transform target;
     public float multipler;
 
-    public float minX = -1000f;
-    public float maxX = 1000f;
-    public float minY = -1000f;
-    public float maxY = 1000f;
-
     void Start()
     {
         
@@ -19,12 +14,6 @@ public class CameraManager : MonoBehaviour
 
     void Update()
     {
-        Vector3 p = Vector3.Lerp(transform.position, target.position + new Vector3(0, 0, -10), Time.deltaTime * multipler);
-        if (p.x < minX) p.x = minX;
-        if (p.x > maxX) p.x = maxX;
-        if (p.y < minY) p.y = minY;
-        if (p.y > maxY) p.y = maxY;
-
-        transform.position = p;
+        transform.position = Vector3.Lerp(transform.position, target.position + new Vector3(0, 0, -10), Time.deltaTime * multipler);
     }
 }
